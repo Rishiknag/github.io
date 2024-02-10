f2f42ce3755ee226061b2ab827a6090387fc2bd9
altomouhit@gmail.com <altomouhit@gmail.com>
2024-02-10 21:11:23 +0530

    Reg/View Against and Compliant Reg / View design new pages

--- conreconApp/assets/js/custom.dataTables.js
+++ conreconApp/assets/js/custom.dataTables.js
@@ -2,10 +2,55 @@ new DataTable('#userTable, #userPowerAttorneyTable, #guardianTable', {
     processing: true,
     "destroy": true,
 	"dom": '<"top"f>rt<"bottom"ilp>',
-		"columnDefs": [{
-			"searchable": false,
-			"orderable": false,
-            //"visible": false,
-			"targets": [12]
-		}],
+	"columnDefs": [{
+		"searchable": false,
+		"orderable": false,
+		//"visible": false,
+		"targets": [12]
+	}],
+});
+
+new DataTable('#againstRegistrationViewTable', {
+    processing: true,
+    "destroy": true,
+	"dom": '<"top"f>rt<"bottom"ilp>',
+	"columnDefs": [{
+		"searchable": false,
+		"orderable": false,
+		//"visible": false,
+		"targets": [9]
+	}],
+});
+new DataTable('#personDataTable', {
+    processing: true,
+    "destroy": true,
+	"dom": '<"top"f>rt<"bottom"ilp>',
+	"columnDefs": [{
+		"searchable": false,
+		"orderable": false,
+		//"visible": false,
+		"targets": [7]
+	}],
+});
+new DataTable('#companyDataTable', {
+	processing: true,
+    "destroy": true,
+	"dom": '<"top"f>rt<"bottom"ilp>',
+	"columnDefs": [{
+		"searchable": false,
+		"orderable": false,
+		//"visible": false,
+		//"targets": [7]
+	}],
+});
+new DataTable('#compliantRegViewTable', {
+	processing: true,
+    "destroy": true,
+	"dom": '<"top"f>rt<"bottom"ilp>',
+	"columnDefs": [{
+		"searchable": false,
+		"orderable": false,
+		//"visible": false,
+		"targets": [10]
+	}],
 });
