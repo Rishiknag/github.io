e7400b256fe9fa0acacb12ee7452e597c6894563
altomouhit@gmail.com <altomouhit@gmail.com>
2024-02-11 23:12:19 +0530

    New pages added in complaint Registration page

--- conreconApp/assets/js/custom.dataTables.js
+++ conreconApp/assets/js/custom.dataTables.js
@@ -29,7 +29,7 @@ new DataTable('#personDataTable', {
 		"searchable": false,
 		"orderable": false,
 		//"visible": false,
-		"targets": [7]
+		"targets": [9]
 	}],
 });
 new DataTable('#companyDataTable', {
@@ -44,6 +44,66 @@ new DataTable('#companyDataTable', {
 	}],
 });
 new DataTable('#compliantRegViewTable', {
+	processing: true,
+    "destroy": true,
+	"dom": '<"top"f>rt<"bottom"ilp>',
+	"columnDefs": [{
+		"searchable": false,
+		"orderable": false,
+		//"visible": false,
+		"targets": [10]
+	}],
+});
+//Divorce or Separation
+new DataTable('#divorceTable', {
+	processing: true,
+    "destroy": true,
+	"dom": '<"top"f>rt<"bottom"ilp>',
+	"columnDefs": [{
+		"searchable": false,
+		"orderable": false,
+		//"visible": false,
+		"targets": [9]
+	}],
+});
+//Custody or Alimony
+new DataTable('#custodyTable', {
+	processing: true,
+    "destroy": true,
+	"dom": '<"top"f>rt<"bottom"ilp>',
+	"columnDefs": [{
+		"searchable": false,
+		"orderable": false,
+		//"visible": false,
+		"targets": [10]
+	}],
+});
+//Dispute Selling Car
+new DataTable('#sellingTable', {
+	processing: true,
+    "destroy": true,
+	"dom": '<"top"f>rt<"bottom"ilp>',
+	"columnDefs": [{
+		"searchable": false,
+		"orderable": false,
+		//"visible": false,
+		"targets": [8]
+	}],
+});
+//Dispute of land
+new DataTable('#landTable', {
+	processing: true,
+    "destroy": true,
+	"dom": '<"top"f>rt<"bottom"ilp>',
+	"columnDefs": [{
+		"searchable": false,
+		"orderable": false,
+		//"visible": false,
+		"targets": [10]
+	}],
+});
+//Dispute well
+new DataTable('#wellTable', {
 	processing: true,
     "destroy": true,
 	"dom": '<"top"f>rt<"bottom"ilp>',
